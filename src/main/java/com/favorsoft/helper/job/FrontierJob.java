6db82e46dae07064e90539df64632afca711d4d9
A <A@A-PC>
2019-01-31 01:55:22 +0900

    메일보내기 진행중

--- src/main/java/com/favorsoft/helper/job/FrontierJob.java
+++ src/main/java/com/favorsoft/helper/job/FrontierJob.java
@@ -76,32 +76,6 @@ public class FrontierJob implements Job {
 						addHelperList.add(req.getHelper());
 						shiftRequestList.remove(random);
 						
-						
-						
-						/*
-						
-						
-						MimeMessage message = javaMailSender.createMimeMessage();
-						
-						try {
-						
-							MimeMessageHelper messageHelper = new MimeMessageHelper(message, true, "UTF-8");
-							messageHelper.setSubject("[Helper] 봉사 안내 메일");
-							String htmlContent = makeMail(project.getProjectName(), yyyyMMdd.format(projectShiftList.get(i).getHelpDate()), project.getDescription(), project.getEducationUrl(), req.getHelper());
-							messageHelper.setText(htmlContent, true);
-							messageHelper.setTo(req.getHelper().getKnoxId() + "@miracom.co.kr");
-							javaMailSender.send(message);
-							
-						} catch (MailException e) {
-							e.printStackTrace();
-							return;
-						} catch (Throwable e) {
-							e.printStackTrace();
-							return;
-						}
-						
-						*/
-						
 					}
 
 					if (shift.getHelpers().size() == project.getMaxHelperCount()) {
@@ -146,20 +120,6 @@ public class FrontierJob implements Job {
 		}
 		
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
 		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
 		Calendar cal = Calendar.getInstance();
@@ -192,14 +152,12 @@ public class FrontierJob implements Job {
 					
 			        try {
 			            MimeMessageHelper messageHelper = new MimeMessageHelper(message, true, "UTF-8");
-			            messageHelper.setSubject("봉사 안내 메일");
+			            messageHelper.setSubject("[HELPER] 봉사 안내 메일");
 			            messageHelper.setTo(helper.getKnoxId() + "@miracom.com");
 			            messageHelper.setFrom("labs.prusoft@gmail.com");
 			            messageHelper.setText(makeMail(p.getProjectName(), yyyyMMdd.format(shift.getHelpDate()), p.getDescription(), p.getEducationUrl(), helperList), true);
-			            // message.setContent(makeMail(p.getProjectName(), yyyyMMdd.format(shift.getHelpDate()), p.getDescription(), p.getEducationUrl(), helperList),"text/html");
 			            javaMailSender.send(message);
 			            
-			 
 			        } catch (MessagingException e) {
 			            e.printStackTrace();
 			        }
@@ -213,71 +171,59 @@ public class FrontierJob implements Job {
 	public String makeMail(String projectName, String helpDate, String description, String url, List<Helper> helperList) {
 		
 		StringBuffer sb = new StringBuffer();
-		sb.append("<!DOCTYPE html>");
-		sb.append("<html>");
-		sb.append("	<head>");
-		sb.append("		<meta charset='utf-8' />");
-		sb.append("		<meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1' />");
-		sb.append("		<meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0'>");
-		sb.append("		<title>Simple Transactional Email</title>");
-
-		// sb.append("		<link rel='stylesheet' type='text/css' href='https://labs.prusoft.space/semantic-ui/semantic.css'>");
-
-		sb.append("	</head>");
-		sb.append("	<body>");
-		sb.append("		<div class='ui raised very padded text container segment' style='top: 30px; color: #5a5252;'>");
-
-		sb.append("			<h2 class='ui header' >");
-		// sb.append("				<img style='width: 100px' class='ui middle aligned tiny image' src='https://labs.prusoft.space/img/Logo.png'>");
-		sb.append("				봉사 안내 메일");
-		sb.append("			</h2>");
-		sb.append("			<p>");
-		sb.append("				<h3>");
-		sb.append("					축하드립니다. ");
-		sb.append("					<br/>봉사자로 선정되었습니다.");
-		sb.append("				</h3>");
-		sb.append("			</p>");
-		sb.append("			<p>");
-		sb.append("				<h3>");
-		sb.append("					봉사 ");
-		sb.append("				</h3>");
-		sb.append("				" + projectName + " ");
-		sb.append("			</p>");
-		sb.append("			<p>");
-		sb.append("				<h3>");
-		sb.append("					일자");
-		sb.append("				</h3>");
-		sb.append("				" + helpDate + " ");
-		sb.append("			</p>");
-		sb.append("			<p>");
-		sb.append("				<h3>");
-		sb.append("					봉사자");
-		sb.append("				</h3>");
+		
+		sb.append("<meta charset='utf-8' />");
+		sb.append("<meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1' />");
+		sb.append("<meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0'>");
+		sb.append("<table align='center' border='0' cellpadding='0' cellspacing='0' width='500' style='color: #5a5252'>");
+		sb.append("	<tr>");
+		sb.append("		<td style='font-size: 0; line-height: 0;' height='10'>&nbsp;</td>");
+		sb.append("	</tr>");
+		sb.append("	<tr>");
+		sb.append("		<td align='center'>");
+		sb.append("			<h1 style='font-family: Impact, Charcoal, sans-serif; font-weight: bold; font-size: 80px; margin:0 0 0 0'>HELPER.</h1>");
+		sb.append("		</td>");
+		sb.append("	</tr>");
+		sb.append("	<tr>");
+		sb.append("		<td align='center'>");
+		sb.append("			<h2 style='margin: 0 0 0 0'>봉사 안내 메일</h2>");
+		sb.append("		</td>");
+		sb.append("	</tr>");
+		sb.append("	<tr>");
+		sb.append("		<td bgcolor='#ffffff' style='padding: 10px 30px 40px 30px;'>");
+		sb.append("			<table border='0' cellpadding='0' cellspacing='0' width='100%'>");
+		sb.append("				<tr>");
+		sb.append("					<td>");
+		sb.append("						<h3 style='margin-bottom: 10px'>봉사</h3>");
+		sb.append("						<div>" + projectName + "</div>");
+								
+		sb.append("						<h3 style='margin-bottom: 10px'>일자</h3>");
+		sb.append("						<div>"+ helpDate +"</div>");
+								
+		sb.append("						<h3 style='margin-bottom: 10px'>봉사자</h3>");
 		
 		for (Helper helper : helperList) {
-			sb.append("				<p>"+helper.getUserName()+" "+helper.getKnoxId()+"@miracom.co.kr</p>");
+			sb.append("						<div>"+helper.getUserName()+" "+helper.getKnoxId()+"@miracom.co.kr</div>");
 		}
-		
-		sb.append("			</p>");
-		sb.append("			<p>");
-		sb.append("				<h3>");
-		sb.append("					내용");
-		sb.append("				</h3>");
-		sb.append("				"+ description +"");
-		sb.append("			</p>		");
-		sb.append("			<p>");
-		sb.append("				<h3>");
-		sb.append("					관련 URL");
-		sb.append("				</h3>");
-		sb.append("				<a href='javascript:void(0)' onclick='window.open(\""+url+"\");'>"+url+"</a>");
-		sb.append("			</p>");
-		sb.append("			<br/>");
-		sb.append("			<a class='fluid ui button' href='javascript:void(0)' onclick='window.open(\"https://labs.prusoft.space\");'>https://labs.prusoft.space</a>");
-
-		sb.append("		</div>");
-
-		sb.append("	</body>");
-		sb.append("</html>");
+								
+		sb.append("						<h3 style='margin-bottom: 10px'>내용 </h3>");
+		sb.append("						<div>"+ description +"</div>");
+								
+		sb.append("						<h3 style='margin-bottom: 10px'>참고 URL</h3>");
+		sb.append("						<div>");
+		sb.append("							<a style='text-decoration:none;' href='"+ url +"' target='_blank'>"+ url +"</a>");
+		sb.append("						</div>");
+		sb.append("					</td>");
+		sb.append("				</tr>");
+		sb.append("			</table>");
+		sb.append("		</td>");
+		sb.append("	</tr>");
+		sb.append("	<tr>");
+		sb.append("		<td bgcolor='#ee4c50' style='text-align: center; padding: 10px 0 10px 0;'>");
+		sb.append("			<a style='font-weight:bold; color: #ffffff; font-size: 15px; text-decoration:none; ' href='http://labs.prusoft.space' target='_blank'>http://labs.prusoft.space</a>");
+		sb.append("		</td>");
+		sb.append("	</tr>");
+		sb.append("</table>");
 		
 		return sb.toString();
 	}
